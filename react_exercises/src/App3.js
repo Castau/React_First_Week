1f8ea8627afd2d070c8366138a173fd9a46c44fe
Camilla <camilla.valerius@gmail.com>
2019-10-28 20:59:06 +0100

    almost done

--- react_exercises/src/App3.js
+++ react_exercises/src/App3.js
@@ -2,6 +2,7 @@
 // 3 Composing Components, Props-1, a
 import React from 'react';
 import PropTypes from "prop-types";
+import { names } from "./file2";
 
 
 // 3 Composing Components, Props-1, b
@@ -15,6 +16,13 @@ export default function App() {
             <Welcome name="Rigmor" />
             <Welcome name="Atramedes" />
             <Welcome name="Deathwing" />
+
+            {/* 3 Composing Components, Props-2, c */}
+            <WelcomePerson person={names[0]} />
+            <WelcomePerson person={names[1]} />
+            <WelcomePerson person={names[2]} />
+
+            {/* This prop gives a runtime error due to the Type check */}
             <Welcome name={45} />
         </div>
     );
@@ -24,3 +32,25 @@ export default function App() {
 Welcome.propTypes = {
     name: PropTypes.string
 };
+
+// 3 Composing Components, Props-2, b
+export function WelcomePerson(props) {
+    const person = props.person;
+    const { firstName, lastName, email } = person;
+
+    return (
+        <div>
+            <p>First Name: {firstName}, Last Name: {lastName}, Email: {email}</p>
+        </div>
+    );
+}
+
+// 3 Composing Components, Props-2, d
+WelcomePerson.propTypes = {
+    person: PropTypes.shape({
+        firstName: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired
+    })
+};
+
--- react_exercises/src/file2.js
+++ react_exercises/src/file2.js
@@ -10,3 +10,14 @@ export default {
 // 2 Object and Array Destructuring, 5
 export const males = ["Varian", "Bolvar"];
 export const females = ["Jaina", "Sylvanas"];
+
+
+// 3 Composing Components, Props-2, a
+export const names = [
+    { firstName: "Arthas", lastName: "Menethil", gender: "Male", email: "lich@king.dk", phone: "12345" },
+    { firstName: "Jaina", lastName: "Proudmore", gender: "female", email: "daughter@ofthesea.dk", phone: "54321" },
+    { firstName: "Sylvanas", lastName: "Windrunner", gender: "Female", email: "lich@queen2be.dk", phone: "12345" }
+];
+// These persons give an error due to the type check if they are pasted in instead of to of the other persons
+    // { firstName: "Jane", email: "j@wonnegut.dk", phone: "12345" },
+    // { firstName: "Jane" },
