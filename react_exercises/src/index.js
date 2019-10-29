5cea3ed0c77890f94b3b6c81a776cd110fd9f0b8
Camilla <camilla.valerius@gmail.com>
2019-10-29 23:30:54 +0100

    ex 2 tuesday

--- react_exercises/src/index.js
+++ react_exercises/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import App2 from './App2';
 import App3 from './App3';
 import App4 from './App4';
+import App5 from './App5';
 
 let app = <App />
 
@@ -16,6 +17,7 @@ const DontUseMeForReal = () => {
             {/* 3 Composing Components, Props-1, c */}
             <a href="/" className="x" id="app3">Monday Exercise 3</a> &nbsp;
             <a href="/" className="x" id="app4">Tuesday Exercise 1</a> &nbsp;
+            <a href="/" className="x" id="app5">Tuesday Exercise 2</a> &nbsp;
          {/* Add as many as you have exercises, but remember className="x" */}
             {app}
         </div>
@@ -33,6 +35,7 @@ function handleSelect(event) {
         case "app2": app = <App2 />; break;
         case "app3": app = <App3 />; break;
         case "app4": app = <App4 initialValue={5} increment={1} />; break;
+        case "app5": app = <App5 />; break;
         default: app = <App />; break;
     }
     ReactDOM.render(<DontUseMeForReal />, document.getElementById('root'));
