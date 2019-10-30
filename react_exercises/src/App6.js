fe96214109d5b7afea043cb713ae1351d758c0ab
Camilla <camilla.valerius@gmail.com>
2019-10-30 17:05:52 +0100

    ex 3 tuesday done

--- react_exercises/src/App6.js
+++ react_exercises/src/App6.js
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from "react";
 
-export default function Jokes() {
-    const [norrisJoke, setNorrisJoke] = useState("");
-    const [dadJoke, setDadJoke] = useState("");
-    const [getNorrisId, setGetNorrisId] = useState("");
+// State and Events with React 3 a-c
+export default function App() {
+    const [norrisJoke, setNorrisJoke] = useState('');
+    const [dadJoke, setDadJoke] = useState('');
+    const [getNorrisId, setGetNorrisId] = useState(false);
 
     useEffect(() => {
-        fetch("norris api")
+        fetch("https://api.chucknorris.io/jokes/random")
             .then(res => res.json())
             .then(data => {
-                setNorrisJoke(data.Value)
+                setNorrisJoke(data.value)
+            })
 
-
-            }), [getNorrisId])
+    }, [getNorrisId]);
 
     useEffect(() => {
         const clear = setInterval(() => {
-            fetch("dad api"), {
+            fetch("https://icanhazdadjoke.com", {
                 headers: {
                     Accept: 'text/plain'
                 }
             })
-            .then(res => res.json())
-            .then(data => {
-                setDadJoke(data)
-            })
-    }, 10000)
-    return () => clearInterval(clear)
-}, [])
+                .then(res => res.text())
+                .then(data => {
+                    setDadJoke(data)
+                })
+        }, 10000)
+        return () => clearInterval(clear)
+    }, []);
+
+    return (
+        <div>
+            <p>Chuck Norris Joke:</p>
+            <p>{norrisJoke}</p>
+            <p>Dad Joke</p>
+            <p>{dadJoke}</p>
+            <button onClick={() => setGetNorrisId(!getNorrisId)}>New Chuck Norris Joke</button>
+        </div>
+    );
 }
 
--- react_exercises/src/index.js
+++ react_exercises/src/index.js
@@ -6,6 +6,7 @@ import App2 from './App2';
 import App3 from './App3';
 import App4 from './App4';
 import App5 from './App5';
+import App6 from './App6';
 
 let app = <App />
 
@@ -18,6 +19,7 @@ const DontUseMeForReal = () => {
             <a href="/" className="x" id="app3">Monday Exercise 3</a> &nbsp;
             <a href="/" className="x" id="app4">Tuesday Exercise 1</a> &nbsp;
             <a href="/" className="x" id="app5">Tuesday Exercise 2</a> &nbsp;
+            <a href="/" className="x" id="app6">Tuesday Exercise 3</a> &nbsp;
          {/* Add as many as you have exercises, but remember className="x" */}
             {app}
         </div>
@@ -36,6 +38,7 @@ function handleSelect(event) {
         case "app3": app = <App3 />; break;
         case "app4": app = <App4 initialValue={5} increment={1} />; break;
         case "app5": app = <App5 />; break;
+        case "app6": app = <App6 />; break;
         default: app = <App />; break;
     }
     ReactDOM.render(<DontUseMeForReal />, document.getElementById('root'));
