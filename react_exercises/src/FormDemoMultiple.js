dff73662b4a599e67d03e106198ad083f977115b
Camilla <camilla.valerius@gmail.com>
2019-10-31 21:14:20 +0100

    Forms and Controlled Components done

---
+++ react_exercises/src/FormDemoMultiple.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+// Forms and Controlled Components 2 a-f
+function ReservationForm() {
+    const initialValue = {
+        payByCreditCard: true,
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        street: "",
+        city: "",
+        zip: "",
+        country: ""
+    };
+    const [reservation, setReservation] = useState(initialValue);
+
+    const handleChange = event => {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+        setReservation({ ...reservation, [name]: value });
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        window.alert(JSON.stringify(reservation));
+        setReservation(initialValue);
+    };
+
+    return (
+        <div><form onChange={handleChange}><label>Pay by Credit Card: </label>
+            <input
+                name="payByCreditCard"
+                type="checkbox"
+                checked={reservation.payByCreditCard} />
+            <br />
+            <input
+                name="firstName"
+                value={reservation.firstName}
+                placeholder="First Name" />
+            <br />
+            <input
+                name="lastName"
+                value={reservation.lastName}
+                placeholder="Last Name" />
+            <br />
+            <input
+                name="email"
+                value={reservation.email}
+                placeholder="E-mail" />
+            <br />
+            <input
+                name="phone"
+                value={reservation.phone}
+                placeholder="Phone" />
+            <br />
+            <input
+                name="street"
+                value={reservation.street}
+                placeholder="Adress" />
+            <br />
+            <input
+                name="city"
+                value={reservation.city}
+                placeholder="City" />
+            <br />
+            <input
+                name="zip"
+                value={reservation.zip}
+                placeholder="Zip code" />
+            <br />
+            <input
+                name="country"
+                value={reservation.country}
+                placeholder="Country" />
+            <br />
+            <button onClick={handleSubmit}>Submit</button>
+        </form>
+            <p>{JSON.stringify(reservation)}</p></div>
+    );
+}
+export default ReservationForm;
--- react_exercises/src/index.js
+++ react_exercises/src/index.js
@@ -10,22 +10,26 @@ import App6 from './App6';
 import App7 from "./ListDemoApp";
 import App8 from "./ListDemoApp2";
 import App9 from "./FormDemo";
+import App10 from "./FormDemoMultiple";
 
 let app = <App />
 
 const DontUseMeForReal = () => {
     return (
-        <div className="App" onClick={handleSelect}>
-            <a href="/" className="x" id="app1">Monday 1</a> &nbsp;
-            <a href="/" className="x" id="app2">Monday 2</a> &nbsp;
-            {/* 3 Composing Components, Props-1, c */}
-            <a href="/" className="x" id="app3">Monday 3</a> &nbsp;
-            <a href="/" className="x" id="app4">Tuesday State and Events 1</a> &nbsp;
-            <a href="/" className="x" id="app5">Tuesday State and Events 2</a> &nbsp;
-            <a href="/" className="x" id="app6">Tuesday State and Events 3</a> &nbsp;
-            <a href="/" className="x" id="app7">Tuesday List and Keys 1</a> &nbsp;
-            <a href="/" className="x" id="app8">Tuesday List and Keys 2</a> &nbsp;
-            <a href="/" className="x" id="app9">Forms and Controlled Components 1</a> &nbsp;
+        <div className="App">
+            <div onClick={handleSelect}>
+                <a href="/" className="x" id="app1">Monday 1</a> &nbsp;
+                <a href="/" className="x" id="app2">Monday 2</a> &nbsp;
+                {/* 3 Composing Components, Props-1, c */}
+                <a href="/" className="x" id="app3">Monday 3</a> &nbsp;
+                <a href="/" className="x" id="app4">Tuesday State and Events 1</a> &nbsp;
+                <a href="/" className="x" id="app5">Tuesday State and Events 2</a> &nbsp;
+                <a href="/" className="x" id="app6">Tuesday State and Events 3</a> &nbsp;
+                <a href="/" className="x" id="app7">Tuesday List and Keys 1</a> &nbsp;
+                <a href="/" className="x" id="app8">Tuesday List and Keys 2</a> &nbsp;
+                <a href="/" className="x" id="app9">Forms and Controlled Components 1</a> &nbsp;
+                <a href="/" className="x" id="app10">Forms and Controlled Components 2</a> &nbsp;
+            </div>
             {app}
         </div>
     );
@@ -47,6 +51,7 @@ function handleSelect(event) {
         case "app7": app = <App7 />; break;
         case "app8": app = <App8 />; break;
         case "app9": app = <App9 />; break;
+        case "app10": app = <App10 />; break;
         default: app = <App />; break;
     }
     ReactDOM.render(<DontUseMeForReal />, document.getElementById('root'));
